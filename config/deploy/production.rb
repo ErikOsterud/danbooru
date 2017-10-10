82a1fed510d33ebedaff117ab44340f24e4c0c27
Erik <erikmosterud@gmail.com>
2017-10-10 09:18:23 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
 set :user, "albert"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "localhost", :roles => %w(web app db), :primary => true, :user => "erik"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
